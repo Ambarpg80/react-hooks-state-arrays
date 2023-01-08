d72296c516b009497449521d458a4e18bb6b8963
Ambar <Ambarpg80@gmail.com>
2023-01-08 13:55:59 -0500

    commit tree 4851f46baad1d0dbbd90d8cd38715475914a520a

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,14 +3,41 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filter, setFilter] = useState("All")
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const addedFood = [...foods, newFood];
+    setFoods(addedFood);
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  function handleLiClick(id){
+    // const removedFood = (foods.filter(food=> food.id !== id))
+    const uptheHeat = foods.map(food => {
+      if(food.id === id){
+      return {...food, heatLevel : food.heatLevel+1};
+      } else { 
+       return food
+      }
+    })
+    setFoods(uptheHeat)
+  }
+
+  function handleFilter(e){
+    setFilter(e.target.value)
+  }
+    const foodFilter = foods.filter(food =>  {
+      if(filter === "All"){
+        return true
+      }else{
+        return food.cuisine === filter
+      }
+    })
+  
+
+
+  const foodList = foodFilter.map((food) => (
+    <li key={food.id} onClick={()=> handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
@@ -18,6 +45,14 @@ function SpicyFoodList() {
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
+      <select name="filter"  onChange={handleFilter}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
+
       <ul>{foodList}</ul>
     </div>
   );
